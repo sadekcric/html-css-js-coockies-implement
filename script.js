282470f8647a2d39114692764756d51a7a661495
sadekcric4 <sadekcric4@gmail.com>
2024-07-08 14:21:11 +0600

    cookies Implementation done

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
+setCookie = (cName, cValue, expDays) => {
+  const date = new Date();
+  date.setTime(date.getTime() + expDays * 24 * 60 * 60 * 1000);
+
+  const expire = `expires= ${date.toUTCString()}`;
+
+  document.cookie = `${cName} = ${cValue}; ${expire}; path=/`;
+};
+
+getCookie = (cName) => {
+  const name = `${cName}=`;
+  const cDecoded = decodeURIComponent(document.cookie);
+
+  const cArr = cDecoded.split(`; `);
+  let value;
+
+  cArr.forEach((val) => {
+    if (val.indexOf(name) === 0) {
+      value = val.substring(name.length);
+    }
+  });
+
+  return value;
+};
+
 document.getElementById("cookies-btn").addEventListener("click", () => {
   document.getElementById("cookies").style.display = "none";
-  setCookie("cookie", true, 30);
+  setCookie("cookie", true, 20);
 });
 
-setCookie = (cName, cValue, cExp) => {};
+window.addEventListener(
+  "load",
+  (cookieMessage = () => {
+    if (!getCookie("cookie")) {
+      document.getElementById("cookies").style.display = "block";
+    }
+  })
+);
